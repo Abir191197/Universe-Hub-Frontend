a063d8ef49b1a18147421f5bbbf9611ce7095bdf
M. Abir Alam <malam19197@bscse.uiu.ac.bd>
2024-06-10 06:52:48 +0600

    image test

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -23,11 +23,7 @@ export default function Navbar() {
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
+              <img className="h-8 w-20" src="https://ibb.co/RB1fPPK" alt="" />
             </a>
           </div>
           <div className="flex lg:hidden">
@@ -50,8 +46,7 @@ export default function Navbar() {
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <div  className="text-sm font-semibold leading-6 text-gray-900">
-            
+            <div className="text-sm font-semibold leading-6 text-gray-900">
               <Link to="/LogIn">
                 Log in <span aria-hidden="true">&rarr;</span>
               </Link>
@@ -68,11 +63,7 @@ export default function Navbar() {
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
-                />
+                <img className="h-8 w-auto" src="" alt="" />
               </a>
               <button
                 type="button"
@@ -95,11 +86,11 @@ export default function Navbar() {
                   ))}
                 </div>
                 <div className="py-6">
-                  <a
-                    href="#"
+                  <Link
+                    to="/LogIn"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                     Log in
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
