673a9353ef7ca29b7dfc2e11dc7b7040bda2504f
M. Abir Alam <malam19197@bscse.uiu.ac.bd>
2024-09-25 00:55:15 +0600

    Render host error

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
+  server: {
+    host: "0.0.0.0", // Bind to all available network interfaces
+    port: 3000, // Specify the port (optional)
+    open: true, // Optional: automatically open the app in the browser
+  },
+});
