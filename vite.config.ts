3a2856dccbbfc19f959f1d26ddcc8e37e0144ea3
M. Abir Alam <malam19197@bscse.uiu.ac.bd>
2024-10-10 13:25:45 +0600

    source hide

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0", // Bind to all available network interfaces
-    port: 5173, // Specify the port (optional)
-    open: true, // Optional: automatically open the app in the browser
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()); // Load environment variables based on the mode
+
+  return {
+    plugins: [react()],
+    server: {
+      host: "0.0.0.0", // Bind to all available network interfaces
+      port: 5173, // Specify the port (optional)
+      open: true, // Automatically open the app in the browser
+    },
+    define: {
+      // Set the global variable based on the environment variable
+      __SOURCE_FILE__: env.VITE_SOURCE_FILE === "false",
+    },
+    build: {
+      sourcemap: env.VITE_SOURCE_FILE === "false" ? false : true, // Disable source maps based on the variable
+    },
+  };
 });
